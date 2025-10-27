5b9e52479d0f3e1cbe90f8d5a2f9aa0929447e9d
SUHYUN YU <suhyun1219yu@gmail.com>
2025-10-27 22:58:38 +0900

    feat(application): print prompts and read inputs

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String names = readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String tryCountRaw = readLine("시도할 횟수는 몇 회인가요?");
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        // 이후 커밋에서 파싱/실행/출력을 구현
+        // 여기서는 입력만 받고 실행 결과 헤더까지만 출력
+    }
+
+    private static String readLine(String prompt) {
+        System.out.println(prompt);
+        return Console.readLine();
     }
 }
